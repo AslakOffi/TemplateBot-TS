449837a19cdf9db8403aee98141da5247d7bc46f
Aslak <51925794+AslakOffi@users.noreply.github.com>
2023-05-12 11:52:29 +0200

    Translation of Ping.ts into English

--- commands/Info/Ping.ts
+++ commands/Info/Ping.ts
@@ -3,13 +3,13 @@ import { RichEmbed } from 'discord.js';
 module.exports = {
   name: 'ping',
   category: 'info',
-  description: "Renvoie la latence et le ping de l'API",
+  description: "Returns the latency and ping of the API",
   run: async (client: any, message: any, args: any) => {
     await message.channel.send(
       new RichEmbed()
         .setTitle('Ping')
-        .setColor('#f9ff35') // Je recommande : https://htmlcolorcodes.com
-        .setDescription(`La latence de l'API est de : **${client.ping}** ms`)
+        .setColor('#f9ff35') // I recommend : https://htmlcolorcodes.com
+        .setDescription(`The latency of the API is : **${client.ping}** ms`)
     );
   }
 };
